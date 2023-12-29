46a74efddb02ccc197310e5c7244332ba417f3a3
taha3rar <taha3rar@gmail.com>
2023-12-29 04:02:11 +0200

    player script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,14 +1,18 @@
+using System.Collections; 
+using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     private Vector2 movement;
-
+    public Animator animator;
+    private SpriteRenderer sp;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -16,11 +20,32 @@ public class PlayerMovement : MonoBehaviour
         // Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        if (movement.magnitude != 0)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+        if (movement.x < 0)
+        {
+            sp.flipX = true;
+        }
+
+
+        else if (movement.x > 0) 
+        {
+            sp.flipX = false;
+        }
     }
 
     void FixedUpdate()
     {
         // Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+       
     }
+
+
 }
